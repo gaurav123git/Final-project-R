85d94ca25fb3525e2747b4d9f62bc417d39e06f5
Gaurav <gaurav123git@gmail.com>
2023-05-14 14:50:27 +0530

    added policy schedule details

---
+++ Insurance/src/main/java/com/velocity/insurance/update/policyscheduledetails/service/PolicyScheduleDetailsService.java
@@ -0,0 +1,9 @@
+package com.velocity.insurance.update.policyscheduledetails.service;
+
+import com.velocity.insurance.update.policyscheduledetails.PolicyScheduleDetails;
+
+public interface PolicyScheduleDetailsService {
+	
+	public PolicyScheduleDetails getPolicyScheduleDetailsById(Integer id);
+
+}
