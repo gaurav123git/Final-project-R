d42e81bf3fac8fc2e23de068e7cde5572ef96fa9
Gaurav <gaurav123git@gmail.com>
2023-05-16 23:12:07 +0530

    updated transection details after paying premium

---
+++ Insurance/src/main/java/com/velocity/insurance/update/transectiondetails/service/TransectionDetailsService.java
@@ -0,0 +1,9 @@
+package com.velocity.insurance.update.transectiondetails.service;
+
+import com.velocity.insurance.update.transectiondetails.TransectionDetails;
+
+public interface TransectionDetailsService {
+	
+	public TransectionDetails getTransectionDetailsById(Integer id);
+
+}
