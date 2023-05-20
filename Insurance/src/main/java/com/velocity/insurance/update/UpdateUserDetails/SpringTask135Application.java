c9327638a21ce1f91f6bdb9f51ded360afa6c02e
RohitDarji <130214138+Rohnaaz91github@users.noreply.github.com>
2023-05-20 10:11:13 +0530

    Update user  details
    task 135

---
+++ Insurance/src/main/java/com/velocity/insurance/update/UpdateUserDetails/SpringTask135Application.java
@@ -0,0 +1,13 @@
+package com.velocity.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTask135Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTask135Application.class, args);
+	}
+
+}
