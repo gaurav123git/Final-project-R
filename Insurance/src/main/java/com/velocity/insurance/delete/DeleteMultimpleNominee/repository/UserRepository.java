d86b120032527444988d61a1a79a6e60e74cf0e0
RohitDarji <130214138+Rohnaaz91github@users.noreply.github.com>
2023-05-21 11:56:42 +0530

    Error Resolve

--- Insurance/src/main/java/com/velocity/insurance/delete/DeleteMultimpleNominee/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.velocity.insurance.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.velocity.insurance.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
-
