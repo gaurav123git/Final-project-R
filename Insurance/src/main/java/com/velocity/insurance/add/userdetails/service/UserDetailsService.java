1c051393643a26e73df13b75caa3095c4f21a4a4
RohitDarji <130214138+Rohnaaz91github@users.noreply.github.com>
2023-05-21 11:45:20 +0530

    Error solve

--- Insurance/src/main/java/com/velocity/insurance/add/userdetails/service/UserDetailsService.java
+++
@@ -1,9 +0,0 @@
-package com.velocity.insurance.add.userdetails.service;
-
-import com.velocity.insurance.add.userdetails.UserDetails;
-
-public interface UserDetailsService {
-	
-	public UserDetails saveUserDetails(UserDetails userDetails);
-
-}
